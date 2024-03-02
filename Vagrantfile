0b5a149eb57e90677065fbec3061d301a08020a9
AirSantix <santiago.villa@siriusxm.com>
2024-03-02 15:44:27 -0300

    Oracle 23c free setup

--- Vagrantfile
+++ Vagrantfile
@@ -81,4 +81,5 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: <<-SHELL
     sh /vagrant/scripts/setup.sh
   SHELL
+
 end
--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -2,20 +2,13 @@
 
 sh /vagrant/scripts/install_os_packages.sh
 
-#sh /vagrant/scripts/oracle_create_database.sh
+mkdir -p ${SOFTWARE_DIR}
+chown -R oracle:oinstall /u01
 
-#mkdir -p ${SOFTWARE_DIR}
-#chown -R oracle:oinstall /u01
-
-#su - oracle -c 'sh /vagrant/scripts/oracle_user_environment_setup.sh'
-#su - oracle -c 'sh /vagrant/scripts/apex_software_installation.sh'
-#su - oracle -c 'sh /vagrant/scripts/ords_software_installation.sh'
-
-#sh /vagrant/scripts/oracle_service_setup.sh
-
+## Postgres Installation
 su - postgres -c 'sh /vagrant/scripts/setup_postgres.sh'
 
-echo "******************************************************************************"
-echo " Download Oracle 23c free binaries" `date`
-echo "******************************************************************************"
-wget -P /vagrant https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
+##echo "******************************************************************************"
+##echo " Download Oracle 23c free binaries" `date`
+##echo "******************************************************************************"
+##wget -P /vagrant https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
