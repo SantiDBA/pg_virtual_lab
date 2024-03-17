627f24f27323189a44b76e95b2eb037333ab66e0
AirSantix <santiago.villa@siriusxm.com>
2024-03-17 17:09:03 -0300

    Enable postgres autostartup

--- scripts/install_os_packages.sh
+++ scripts/install_os_packages.sh
@@ -28,6 +28,7 @@ echo "export PATH=\$PATH:/usr/pgsql-15/bin" > /var/lib/pgsql/.pgsql_profile
 /usr/pgsql-15/bin/postgresql-15-setup initdb
 echo "listen_addresses = '*'" >> /var/lib/pgsql/15/data/postgresql.conf
 echo "host    all             all             samenet           scram-sha-256" >> /var/lib/pgsql/15/data/pg_hba.conf
+systemctl enable postgresql-15
 systemctl start postgresql-15
 
 echo "******************************************************************************"
