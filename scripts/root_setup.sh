df4ba25bf8dfd7cbab748715e588583357dd8dc8
AirSantix <santiago.villa@siriusxm.com>
2024-04-03 23:32:00 -0300

    Update root_setup.sh

--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -1,18 +1,5 @@
 . /vagrant/config/install.env
 
-mkdir -p ${SOFTWARE_DIR}
-chown -R oracle:oinstall /u01
-
-# set environment variables
-cat >> /home/oracle/.bashrc << EOF
-export ORACLE_BASE=/opt/oracle
-export ORACLE_HOME=/opt/oracle/product/23c/dbhomeFree
-export ORACLE_SID=FREE
-export PATH=\$PATH:\$ORACLE_HOME/bin
-EOF
-
-chown oracle:oinstall /home/oracle/.bashrc
-
 ## Oracle Installation
 sudo bash -c 'sh /vagrant/scripts/setup_oracle.sh'
 
@@ -29,6 +16,19 @@ su - oracle -c 'sh /vagrant/scripts/setup_swingbench.sh'
 ##echo " Download Oracle 23c free binaries" `date`
 ##echo "******************************************************************************"
 wget -P /vagrant https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
+
+mkdir -p ${SOFTWARE_DIR}
+chown -R oracle:oinstall /u01
+
+# set environment variables
+cat >> /home/oracle/.bashrc << EOF
+export ORACLE_BASE=/opt/oracle
+export ORACLE_HOME=/opt/oracle/product/23c/dbhomeFree
+export ORACLE_SID=FREE
+export PATH=\$PATH:\$ORACLE_HOME/bin
+EOF
+
+chown oracle:oinstall /home/oracle/.bashrc
 
 echo "******************************************************************************"
 echo "** Your virtual environment is ready now.                                     "
