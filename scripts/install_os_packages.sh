e69076db70599be9b111ef17b23f391547fb99d8
AirSantix <santiago.villa@siriusxm.com>
2024-04-06 00:27:54 -0300

    Update install_os_packages.sh

--- scripts/install_os_packages.sh
+++ scripts/install_os_packages.sh
@@ -25,7 +25,7 @@ dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_6
 dnf -qy module disable postgresql
 
 # Check the boolean variable
-if [ "$INSTALL_POSTGRES" -eq true ]; then
+if [[ "${INSTALL_POSTGRES,,}" == 'true' ]]; then
     dnf install -y postgresql15-server
     echo "export PATH=\$PATH:/usr/pgsql-15/bin" > /var/lib/pgsql/.pgsql_profile
     /usr/pgsql-15/bin/postgresql-15-setup initdb
