c13e976102824b21c34827e1bb022c972732cefd
AirSantix <santiago.villa@siriusxm.com>
2024-04-10 11:56:10 -0300

    Modify installation
    Remove postgres installation by default.
    Exclude tnsnames setup by default.

---
+++ dba_academy/labs/setup_tnsnames.sh
@@ -0,0 +1,17 @@
+## Setup tnsnames for future FREE DB
+mkdir -p /opt/oracle/product/23c/dbhomeFree/network/admin
+touch /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
+chown oracle:oinstall /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
+chmod o+r /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
+
+# add tnsnames.ora entry for PDB
+cat >> /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora << EOF
+FREEPDB1 =
+  (DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = $ORACLE_HOSTNAME)(PORT = $LISTENER_PORT))
+    (CONNECT_DATA =
+      (SERVER = DEDICATED)
+      (SERVICE_NAME = FREEPDB1)
+    )
+  )
+EOF
--- scripts/setup_oracle.sh
+++ scripts/setup_oracle.sh
@@ -59,24 +59,6 @@ if [[ ! -f /vagrant/"${db_installer}" ]]; then
        https://download.oracle.com/otn-pub/otn_software/db-free/"${db_installer}"
 fi
 
-## Setup tnsnames for future FREE DB
-mkdir -p /opt/oracle/product/23c/dbhomeFree/network/admin
-touch /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
-chown oracle:oinstall /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
-chmod o+r /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
-
-# add tnsnames.ora entry for PDB
-cat >> /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora << EOF
-FREEPDB1 =
-  (DESCRIPTION =
-    (ADDRESS = (PROTOCOL = TCP)(HOST = $ORACLE_HOSTNAME)(PORT = $LISTENER_PORT))
-    (CONNECT_DATA =
-      (SERVER = DEDICATED)
-      (SERVICE_NAME = FREEPDB1)
-    )
-  )
-EOF
-
 echo 'INSTALLER: Database software downloaded'
 
 # run user-defined post-setup scripts
