8308cfee98e787d197e9040fa4d156edc29f85e3
AirSantix <santiago.villa@siriusxm.com>
2024-04-12 10:20:12 -0300

    Setup lab

---
+++ dba_academy/setup/destroy_lab_MAC.sh
@@ -0,0 +1,5 @@
+cd pg_virtual_lab-main
+vagrant destroy
+cd ..
+rm -rf pg_virtual_lab-main
+rm -f syscon_lab.zip
---
+++ dba_academy/setup/setup_lab_MAC.sh
@@ -0,0 +1,14 @@
+echo "Installing VirtualBox..."
+brew install --cask virtualbox
+echo "Installing VirtualBox... DONE"
+echo "Installing Vagrant..."
+brew tap hashicorp/tap
+brew install hashicorp/tap/hashicorp-vagrant
+echo "Installing Vagrant... DONE"
+echo "Downloading Virtual Machine..."
+wget --output-document=syscon_lab.zip https://github.com/SantiDBA/pg_virtual_lab/archive/refs/heads/main.zip
+echo "Downloading Virtual Machine... DONE"
+echo "Unzipin labs..."
+unzip syscon_lab.zip
+cd pg_virtual_lab-main
+vagrant up
