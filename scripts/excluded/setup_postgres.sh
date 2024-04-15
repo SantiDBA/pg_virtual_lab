6d8ee1538f22e996e2d69f1d7aad2219f9e45281
AirSantix <santiago.villa@siriusxm.com>
2024-04-15 17:32:22 -0300

    lab 4 setup

--- scripts/excluded/setup_postgres.sh
+++ scripts/excluded/setup_postgres.sh
@@ -3,5 +3,5 @@
 psql -c "ALTER USER postgres PASSWORD 'postgres';"
 psql -c "create database mytestdb;" 
 
-pgbench -i mytestdb
+pgbench -i mytestdb -s 100
 ##nohup pgbench -c 10 -T 999999999 mytestdb &
